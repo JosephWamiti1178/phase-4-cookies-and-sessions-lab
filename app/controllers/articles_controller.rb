5b4d5c14ea16b74f1414c1bdb47c5ec2a456db82
Joseph <wamiti8711@gmail.com>
2023-03-25 04:24:52 -0400

    cookies and sessions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,25 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    # article = Article.find(params[:id])
+    # render json: article
+
+    #check if this is the first user first view and initialize the session counter
+    session[:page_views] ||= 0
+
+    #increment the session count for each view
+    session[:page_views] += 1
+
+    #check if the user has viewed more than 3 pages and render an error
+    if session[:page_views] > 3
+      render json: { error: "Maximum pageview limit reached" }, status: :unauthorized
+    else
+      #otherwise render the article data
+      # article = Article.find(params[:id])
+      # render json: articles, each_serializer: ArticleListSerializer
+      article = Article.find(params[:id])
+      render json: article
+    end
   end
 
   private
